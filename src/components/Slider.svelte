7ee9d99e4743bdca1e94fa9882b41ea2b28e00a2
nurintaaan <intannnalatas@gmail.com>
2021-02-03 23:56:46 +0700

    add slider functionality

--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -1,15 +1,19 @@
 <!-- TODO: make props -->
 <script>
 	// export let data;
+	import { Swiper, SwiperSlide } from 'swiper/svelte';
+	import SwiperCore, { Navigation } from 'swiper';
+
+	SwiperCore.use([Navigation]);
 
 	let data = [
-		{id: 1},
-		{id: 2},
-		{id: 3},
-		{id: 4},
+		{src: './images/project_1.png'},
+		{src: './images/project_2.png'},
+		{src: './images/project_3.png'},
+		{src: './images/project_4.png'},
 	];
 
-	let activeIndex = 2;
+	let activeIndex = 0;
 
 	$: isActive = (index) => (activeIndex === index);
 	$: isActiveAround = (index) => (activeIndex-1 === index || activeIndex+1 === index);
@@ -28,12 +32,27 @@
 	}
 </script>
 
+<div>
+	<!-- TODO: change navigation color, remove navigation on small screen -->
+	<Swiper
+	   slidesPerView={1}
+	   on:slideNextTransitionEnd={swipeRight}
+	   on:slidePrevTransitionEnd={swipeLeft}
+	   on:swiper={(e) => (window.swiper = e.detail[0])}
+	   navigation
+	 >
+	 	{#each data as datum , i}
+	 		<SwiperSlide isVisible={activeIndex === i}>
+		    	<img 
+					src="{datum.src}" 
+					alt="Project" 
+					class="w-3/4 lg:w-1/2 m-auto my-4 shadow-md" 
+				/>
+		    </SwiperSlide>
+		{/each}
+	</Swiper>	
+</div>
 <div class="flex flex-col justify-center">
-	<img 
-		src="{`./images/project_${activeIndex+1}.png`}" 
-		alt="Project" 
-		class="w-3/4 lg:w-1/2 m-auto my-4 shadow-md" 
-	/>		
 	<div 
 		class="inline-flex m-auto"
 	>
@@ -41,29 +60,15 @@
 			<div 
 				class:slider--active="{isActive(i)}" 
 				class:slider--big="{isActiveAround(i)}" 
-				class:slider="{isNotActive(i)}" 
+				class:slider="{isNotActive(i)}"
 			 />
 
 		{/each}
 	</div>
-	<!-- TODO: make it like arrow -->
-	<div class="inline-flex m-auto mt-4">
-		<button 
-			class="p-4 bg-secondary mr-4 text-white rounded" 
-			on:click="{() => swipeLeft()}"
-		>
-			LEFT
-		</button>
-		<button 
-			class="p-4 bg-secondary text-white rounded" 
-			on:click="{() => swipeRight()}"
-		> 
-			RIGHT
-		</button>
-	</div>
 </div>
 
 <style>
+
 	.slider--active {
 		width: 18px;
 		height: 18px;
@@ -88,5 +93,8 @@
 		margin-right: 2px;
 		border-radius: 50%;
 		background-color: #C5C5C5;
+	}
+	.swiper-button-next {
+		color: #9CA27B;
 	}
 </style>
