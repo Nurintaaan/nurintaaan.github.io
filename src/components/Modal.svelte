142c153062fc267b9be0c7da43e51b7f4d4d0fb1
nurintaaan <intannnalatas@gmail.com>
2021-02-09 23:06:40 +0700

    add project constant

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -16,20 +16,32 @@
       <div class="absolute inset-0 bg-blackFull opacity-50" on:click={dismiss}></div>
     </div>
 
+    <!-- TODO: center the modal -->
     <!-- This element is to trick the browser into centering the modal contents. -->
     <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
     <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
       <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
         <div class="sm:flex sm:items-start">
-          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-              {data.title}
-            </h3>
-            <p class="text-sm text-gray-500">
-              {data.startDate} - {data.endDate}
-            </p>
+          <div class="mt-3 sm:mt-0 sm:ml-4 text-left">
+            <div class="mb-4">
+              <h3 class="lg:text-2xl text-base leading-6 font-medium text-gray-900"  id="modal-headline">
+                {data.title}
+              </h3>
+              {#if data.startDate && data.endDate}
+                <p class="text-xs lg:text-sm text-gray-500">
+                  {data.startDate} - {data.endDate}
+                </p>
+              {/if}
+              {#if data.link}
+                <div class="mt-4">
+                  <a href={data.link}> 
+                    {data.link}
+                  </a>
+                </div>
+              {/if}
+            </div>
             <div class="mt-2">
-              <ul class="text-gray-500 list-disc ml-4">
+              <ul class="text-xs lg:text-base text-gray-500 list-disc ml-4">
                 {#each data.responsibilities as responsibility}
                   <li>{responsibility}</li>
                 {/each}
@@ -37,7 +49,7 @@
             </div>
             <div class="mt-8 mb-2">
               {#each data.skills as skill}
-                <span class="rounded-full py-1 px-3" style="background-color: {skill.background}; color: {skill.color}">
+                <span class="lg:text-base text-sm rounded-full py-1 px-3 mr-2" style="background-color: {skill.background}; color: {skill.color}">
                   {skill.name}
                 </span>
               {/each}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,9 +3,9 @@ const projects = {
 	    title: 'Svelte Boilerplate',
 	    startDate: '',
 	    endDate: '',
+	    link: 'https://github.com/Nurintaaan/svelte-boilerplate',
 	    responsibilities: [
 	      'Boilerplate for light to medium scale project. Start your project within a second with this boilerplate',
-	      'https://github.com/Nurintaaan/svelte-boilerplate'
 	    ],
 	    skills: [
 	      {
@@ -14,6 +14,80 @@ const projects = {
 	        background: '#ff3e00',
 	      }
 	    ]
-	 }
+	 },
+	 println: {
+	    title: 'Println',
+	    startDate: '',
+	    endDate: '',
+	    link: 'https://gitlab.com/PPL2018csui/Kelas-C/PPL2018-C3',
+	    responsibilities: [
+	      'Println is an automatic print webplatform with cashless payment system that will help user to print their files by just uploading it to the web.',
+	      'Create wireframe and mock up for the web',
+	      'Create admin and user facing page for the project',
+	    ],
+	    skills: [
+	      {
+	        name: 'React',
+	        color: '#61DAFB',
+	        background: '#222222',
+	      },
+	      {
+	        name: 'Redux',
+	        color: '#FEFEFE',
+	        background: '#764ABD',
+	      },
+	      {
+	        name: 'Jest',
+	        color: '#FFFFFF',
+	        background: '#C21325',
+	      },
+	      {
+	        name: 'Figma',
+	        color: '#FFFFFF',
+	        background: '#000000',
+	      }
+	    ]
+	 },
+	 sevy: {
+	    title: 'Sevy',
+	    startDate: '',
+	    endDate: '',
+	    link:'https://line.me/R/ti/p/IO_w-wrX9m',
+	    responsibilities: [
+	      'Sevy Bot is a first private student’s financial assistant which help you to manage about your financial problem.',
+	      'Creating logo, mascot and weekly publications for Sevy and increase more than twice adders since the first MVP.',
+	    ],
+	    skills: [
+	      {
+	        name: 'Adobe Illustrator',
+	        color: '#330000',
+	        background: '#FF9A00',
+	      }
+	    ]
+	 },
+	 learningBuddies: {
+	    title: 'Learning Buddies',
+	    startDate: '',
+	    endDate: '',
+	    link: 'https://www.youtube.com/watch?v=wYt4k_jZD_c',
+	    responsibilities: [
+	      'Learning buddies is a mobile application that helps people who has the same learning interest study together anytime and anyplace. With learning buddies you can find nearby events, and even find your learning partner using its feature.',
+	      'Create UI with android studio',
+	      'Create design and publication for the app',
+	    ],
+	    skills: [
+	      {
+	        name: 'Android Studio',
+	        color: '#ffffff',
+	        background: '#ff3e00',
+	      },
+	      {
+	        name: 'XML',
+	        color: '#ffffff',
+	        background: '#000000',
+	      }
+	    ]
+	 },
+
 }
 export default projects;
